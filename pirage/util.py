4a6dcf2af086b3e3b8a68b779d75732680fb64dd
Brian Parma <bparma@bdatech.com>
2015-06-29 10:28:01 -0700

    add some docstrings

--- pirage/util.py
+++ pirage/util.py
@@ -4,18 +4,26 @@ from contextlib import contextmanager
 
 @contextmanager
 def shelf(path):
+    '''
+    Contextmanager for shelve.open
+    '''
     s = shelve.open(path)
     yield s
     s.close()
 
 @contextmanager
 def tempdir():
+    '''
+    Contextmanager for tempfile.mkdtemp
+    '''
     path = tempfile.mkdtemp()
     yield path
     shutil.rmtree(path)
 
-
 class AttrDict(dict):
+    '''
+    dict subclass that adds attribute access for keys.
+    '''
     def __init__(self, *args, **kwargs):
         # super().__init__(*args, **kwargs)
         super(AttrDict, self).__init__(*args, **kwargs)
