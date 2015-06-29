0cf001b99ca4e87b542bc63cf9469d78a0038afd
Brian Parma <execrable@gmail.com>
2015-06-28 18:48:34 -0700

    add fab file tasks

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,25 @@ from fabric.contrib.project import rsync_project
 
 env.use_ssh_config = True
 
+env.hosts = ['pirage']
+
+project = 'https://github.com/bj0/pirage.git'
+
+@task
+def clone():
+    with cd('~/'):
+        run('git clone {}'.format(project))
+
+@task
+def update():
+    with cd('~/pirage'):
+        run('git pull')
+
+@task
+def restart():
+    sudo("supervisorctl restart pirage")
+    sudo("supervisorctl status")
+
+@task
+def check():
+    sudo("supervisorctl status")
