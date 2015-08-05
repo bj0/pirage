b64817caa36233d1525c8466adfd64b0bc757be4
Brian Parma <bparma@bdatech.com>
2015-08-05 16:18:47 -0700

    rearrange elements add alt image for missing garage image adjust button styling and use table for status split individual buttons

--- pirage/static/js/script.jsx
+++ pirage/static/js/script.jsx
@@ -37,14 +37,10 @@ var GarageButton = React.createClass({
   },
   render: function() {
     return (
-      <div className="round-button">
-        <div className="round-button-circle">
-          <button id="button" className="pure-round center"
-            onClick={this.handleClick}>
-            { this.props.garage_open ? "CLOSE" : "OPEN"}
-          </button>
-        </div>
-      </div>
+      <button id="button" className="pure-button pure-round center"
+        onClick={this.handleClick}>
+        { this.props.garage_open ? "CLOSE" : "OPEN"}
+      </button>
     );
   }
 });
@@ -53,28 +49,22 @@ var GarageButton = React.createClass({
 var StatusDisplay = React.createClass({
   render: function() {
     return (
-      <div>
-      <div className="pure-g">
-        <div className="pure-u-1-4"></div>
-        <div className="pure-u-1-4 tcenter">
-          { this.props.garage_open ? "OPEN" : "CLOSED"} for:
-        </div>
-        <div className="pure-u-1-4">
-          <div className="l-box">{this.props.last_change}</div>
-        </div>
-        <div className="pure-u-1-4"></div>
-      </div>
-      <div className="pure-g">
-        <div className="pure-u-1-4"></div>
-        <div className="pure-u-1-4 tcenter ">
-          Last Motion:
-        </div>
-        <div className="pure-u-1-4">
-          <div className="l-box">{this.props.last_motion}</div>
-        </div>
-        <div className="pure-u-1-4"></div>
-      </div>
-      </div>
+      <table className="pure-table" style={{marginTop: '20px'}}>
+        <tbody>
+          <tr>
+            <td>{ this.props.garage_open ? "open" : "closed" } for:</td>
+            <td>{this.props.last_change}</td>
+          </tr>
+          <tr>
+            <td>last motion:</td>
+            <td>{this.props.last_motion}</td>
+          </tr>
+          <tr>
+            <td>temp:</td>
+            <td>{this.props.temperature} &deg;C</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 });
@@ -83,7 +73,9 @@ var StatusDisplay = React.createClass({
 var GarageImage = React.createClass({
   getInitialState: function() {
     return {
-      image_src: "http://admin:taco@10.10.10.102/image/jpeg.cgi"
+      image_src: "http://admin:taco@10.10.10.102/image/jpeg.cgi",
+      altimage_src: "/static/images/loading.png",
+      loaded: false
     };
   },
   // componentDidMount: function() {
@@ -95,19 +87,31 @@ var GarageImage = React.createClass({
     console.log(url);
     this.setState({ image_src: url })
   },
+  handleError: function() {
+    // called when image fails to load
+    console.log("cannot get image from webcam");
+    this.setState({ altimage_src: "/static/images/fail.png"});
+  },
+  handleLoad: function() {
+    // called when image is loaded
+    this.setState({ loaded: true });
+  },
   render: function() {
     return (
-      <div>
-        <img className="center" src={this.state.image_src}
+      <div style={{margin: '10px'}}>
+        <img className="center" src={this.state.image_src} hidden={ !this.state.loaded }
+          onLoad={this.handleLoad}
+          onError={this.handleError}
           onClick={this.handleClick} />
+        <img className="center" src={this.state.altimage_src} hidden={ this.state.loaded }
+          onClick={this.handleClick}/>
       </div>
     );
   }
 });
 
-
 // toggle buttons for controlling features
-var ControlButtons = React.createClass({
+var DweetButton = React.createClass({
   render: function() {
     var dweet;
     if (this.props.dweet_enabled) {
@@ -119,29 +123,55 @@ var ControlButtons = React.createClass({
               </span>;
     }
     return (
-      <div className="pure-g">
-      <div className='pure-u-1-3 tcenter'>
-        <button className="pure-button"
-          onClick={this.props.handleLockClick}>
-          <i className={this.props.locked ? "fa fa-lock" : "fa fa-unlock"}></i>
-          &nbsp; LOK
-        </button>
+      <button className="pure-button fa-button"
+        onClick={this.props.handleDweetClick}>
+        {dweet}
+      </button>
+    )
+  }
+});
+
+var LockButton = React.createClass({
+  render: function() {
+    return (
+      <button className="pure-button fa-button"
+        onClick={this.props.handleLockClick}>
+        <i className={this.props.locked ? "fa fa-lock" : "fa fa-unlock"}></i>
+      </button>
+    )
+  }
+});
+
+var PirButton = React.createClass({
+  render: function() {
+    return (
+      <button className="pure-button fa-button"
+        onClick={this.props.handlePirClick}>
+        <i className={this.props.pir_enabled ? "fa fa-eye" : "fa fa-eye-slash" }></i>
+      </button>
+    )
+  }
+});
+
+var ControlButtons = React.createClass({
+  render: function() {
+    return (
+      <div style={{textAlign:'right', marginTop: '10px'}}>
+        <LockButton {...this.props}/><br/>
+        <PirButton {...this.props} /><br/>
+        <DweetButton {...this.props}/>
       </div>
-      <div className='pure-u-1-3 tcenter'>
-        <button className="pure-button"
-          onClick={this.props.handlePirClick}>
-          <i className={this.props.pir_enabled ? "fa fa-eye" : "fa fa-eye-slash" }></i>
-          &nbsp;PIR
-        </button>
-      </div>
-      <div className='pure-u-1-3 tcenter'>
-        <button className="pure-button"
-          onClick={this.props.handleDweetClick}>
-          {dweet}
-          &nbsp;dweet
-        </button>
-      </div>
-    </div>
+    //   <div className="pure-g">
+    //   <div className='pure-u-1-3 tcenter'>
+    //     <LockButton {...this.props}/>
+    //   </div>
+    //   <div className='pure-u-1-3 tcenter'>
+    //     <PirButton {...this.props} />
+    //   </div>
+    //   <div className='pure-u-1-3 tcenter'>
+    //     <DweetButton {...this.props}/>
+    //   </div>
+    // </div>
     );
   }
 });
@@ -163,7 +193,6 @@ var Garage = React.createClass({
     eventSource.onmessage = function (e) {
         this.updateState(JSON.parse(e.data));
         this.setState({log:e.data});
-        // $("#log").html(e.data);
     }.bind(this);
   },
   updateState: function(data) {
@@ -174,6 +203,7 @@ var Garage = React.createClass({
       dweet_enabled: data.dweet_enabled,
       pir_enabled: data.pir_enabled,
       locked: data.locked,
+      temperature: data.temp,
       log: "hi"
     })
   },
@@ -204,20 +234,29 @@ var Garage = React.createClass({
   render: function() {
     return (
       <div>
+        <div className="pure-g">
+          <div className="pure-u-1-3">
+            <ControlButtons
+              dweet_enabled={this.state.dweet_enabled}
+              pir_enabled={this.state.pir_enabled}
+              locked={this.state.locked}
+              handlePirClick={this.handlePirClick}
+              handleDweetClick={this.handleDweetClick}
+              handleLockClick={this.handleLockClick} />
+          </div>
+          <div className="pure-u-1-3">
+          <GarageImage />
+          </div>
+          <div className="pure-u-1-3">
+          <StatusDisplay
+            garage_open={this.state.garage_open}
+            last_change={this.state.last_change}
+            last_motion={this.state.last_motion}
+            temperature={this.state.temperature} />
+        </div>
+        </div>
       <GarageButton
         garage_open={this.state.garage_open} />
-      <StatusDisplay
-        garage_open={this.state.garage_open}
-        last_change={this.state.last_change}
-        last_motion={this.state.last_motion} />
-      <ControlButtons
-        dweet_enabled={this.state.dweet_enabled}
-        pir_enabled={this.state.pir_enabled}
-        locked={this.state.locked}
-        handlePirClick={this.handlePirClick}
-        handleDweetClick={this.handleDweetClick}
-        handleLockClick={this.handleLockClick} />
-      <GarageImage />
       <div id="log" style={{fontFamily: 'courier', fontSize: '0.75em' }}>{this.state.log}</div>
       </div>
     );
