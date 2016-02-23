478cb84f982be7292dbb9d764b231d9a45c20b0b
Brian Parma <bparma@bdatech.com>
2016-02-22 17:07:07 -0700

    update tests to use asyncio

--- pirage/tests/test_garage.py
+++ pirage/tests/test_garage.py
@@ -1,16 +1,18 @@
-from __future__ import absolute_import
+# from __future__ import absolute_import
 
 import pytest
-import gevent
+# import gevent
+import asyncio as aio
 import mock
 
 from pirage.util import AttrDict
 from pirage.garage import Garage
-from .fastsleep import fastsleep, fancysleep
+from .aiofastsleep import fastsleep, fancysleep
 
-def test_open_when_locked(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_open_when_locked(fancysleep, monkeypatch):
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     toggle = mock.MagicMock()
     g = Garage(toggle)
@@ -20,20 +22,21 @@ def test_open_when_locked(fancysleep, monkeypatch):
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # open door
         g.update(AttrDict(pir=False, mag=False))
 
         # an hour later...
-        gevent.sleep(60*60)
+        await aio.sleep(60*60)
 
         # no auto-close
         assert not toggle.called
 
-def test_lock_after_open(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_lock_after_open(fancysleep, monkeypatch):
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     toggle = mock.MagicMock()
     g = Garage(toggle)
@@ -42,25 +45,26 @@ def test_lock_after_open(fancysleep, monkeypatch):
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # open door
         g.update(AttrDict(pir=False, mag=False))
 
         # after 5 minutes
-        gevent.sleep(5*60)
+        await aio.sleep(5*60)
 
         g.lock()
 
         # an hour later...
-        gevent.sleep(60*60)
+        await aio.sleep(60*60)
 
         # no auto-close
         assert not toggle.called
 
-def test_unlock_while_closed(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_unlock_while_closed(fancysleep, monkeypatch):
         monkeypatch.setattr('time.time', lambda: fancysleep.now)
-        fancysleep.patch('pirage.garage.sleep')
+        #fancysleep.patch('pirage.garage.sleep')
 
         toggle = mock.MagicMock()
         g = Garage(toggle)
@@ -70,19 +74,20 @@ def test_unlock_while_closed(fancysleep, monkeypatch):
         g.update(AttrDict(pir=False, mag=True))
 
         with fancysleep:
-            gevent.sleep(5)
+            await aio.sleep(5)
 
             g.lock(False)
 
             # an hour later...
-            gevent.sleep(60*60)
+            await aio.sleep(60*60)
 
             # no auto-close
             assert not toggle.called
 
-def test_unlock_while_open(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_unlock_while_open(fancysleep, monkeypatch):
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     toggle = mock.MagicMock()
     g = Garage(toggle)
@@ -92,18 +97,18 @@ def test_unlock_while_open(fancysleep, monkeypatch):
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # open door
         g.update(AttrDict(pir=False, mag=False))
 
         # after 5 minutes...
-        gevent.sleep(5*60)
+        await aio.sleep(5*60)
 
         g.lock(False)
 
         # 20 min later...
-        gevent.sleep(20*60)
+        await aio.sleep(20*60)
 
         # auto-close
         assert toggle.called
@@ -128,11 +133,12 @@ def test_data(monkeypatch):
     assert data.last_pir_str == '10 sec'
     assert data.last_mag_str == '5 min'
 
-def test_garage_autoclose(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_garage_autoclose(fancysleep, monkeypatch):
     toggle = mock.MagicMock()
     notify = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     g.notify = notify
@@ -140,27 +146,28 @@ def test_garage_autoclose(fancysleep, monkeypatch):
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # open door
         g.update(AttrDict(pir=False, mag=False))
 
-        gevent.sleep(g.close_delay-1)
+        await aio.sleep(g.close_delay-1)
         assert not toggle.called
 
         # make sure warning went out
         assert mock.call('closing garage in {} minutes!'
             .format(g.close_warning/60)) in notify.mock_calls
-        gevent.sleep(1)
+        await aio.sleep(1)
 
         # garage door close called
         assert toggle.called
 
-def test_garage_no_autoopen(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_garage_no_autoopen(fancysleep, monkeypatch):
     toggle = mock.MagicMock()
     notify = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     g.notify = notify
@@ -168,65 +175,66 @@ def test_garage_no_autoopen(fancysleep, monkeypatch):
     g.update(AttrDict(pir=False, mag=False))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # close door
         g.update(AttrDict(pir=False, mag=True))
 
-        gevent.sleep(g.close_delay-1)
+        await aio.sleep(g.close_delay-1)
         assert not toggle.called
 
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # garage door close not called
         assert not toggle.called
 
-        gevent.sleep(1000)
+        await aio.sleep(1000)
 
         # garage door close not called
         assert not toggle.called
 
-
-def test_garage_autoclose_delayed_by_motion(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_garage_autoclose_delayed_by_motion(fancysleep, monkeypatch):
     toggle = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     # initial setup
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # open door
         g.update(AttrDict(pir=False, mag=False))
 
         # advance to 10s before auto-close
-        gevent.sleep(g.close_delay-10)
+        await aio.sleep(g.close_delay-10)
 
         # signal movement
         g.update(AttrDict(pir=True, mag=False))
-        gevent.sleep()
+        await aio.sleep()
         g.update(AttrDict(pir=False, mag=False))
 
         # wait till past previous auto-close
-        gevent.sleep(30)
+        await aio.sleep(30)
 
         assert not toggle.called
 
         # advanced past motion delay to trigger auto-close
-        gevent.sleep(g.motion_delay-30)
+        await aio.sleep(g.motion_delay-30)
 
         # garage door close called
         assert toggle.called
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize('pir',[True,False])
-def test_garage_notify_on_left_open(fancysleep, monkeypatch, pir):
+async def test_garage_notify_on_left_open(fancysleep, monkeypatch, pir):
     toggle = mock.MagicMock()
     notify = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     g.notify = notify
@@ -235,23 +243,23 @@ def test_garage_notify_on_left_open(fancysleep, monkeypatch, pir):
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # open door
         g.update(AttrDict(pir=pir, mag=False))
 
-        gevent.sleep(g.notify_delay-1)
+        await aio.sleep(g.notify_delay-1)
         assert mock.call("Garage is still open after {mag_open} minutes!") not in notify.mock_calls
-        gevent.sleep(1)
+        await aio.sleep(1)
         notify.assert_called_with("Garage is still open after {mag_open} minutes!")
 
-
+@pytest.mark.asyncio
 @pytest.mark.parametrize('mag',[True,False])
-def test_garage_notify_on_motion(fastsleep, monkeypatch, mag):
+async def test_garage_notify_on_motion(fastsleep, monkeypatch, mag):
     toggle = mock.MagicMock()
     notify = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fastsleep.now)
-    fastsleep.patch('pirage.garage.sleep')
+    #fastsleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     g.notify = notify
@@ -264,12 +272,13 @@ def test_garage_notify_on_motion(fastsleep, monkeypatch, mag):
 
     notify.assert_called_with('motion!')
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize('pir',[True,False])
-def test_garage_notify_on_door_change(fastsleep, monkeypatch, pir):
+async def test_garage_notify_on_door_change(fastsleep, monkeypatch, pir):
     toggle = mock.MagicMock()
     notify = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fastsleep.now)
-    fastsleep.patch('pirage.garage.sleep')
+    #fastsleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     g.notify = notify
@@ -282,11 +291,12 @@ def test_garage_notify_on_door_change(fastsleep, monkeypatch, pir):
 
     assert mock.call('door open!') in notify.mock_calls
 
-def test_garage_notify_canceled_after_close(fancysleep, monkeypatch):
+@pytest.mark.asyncio
+async def test_garage_notify_canceled_after_close(fancysleep, monkeypatch):
     toggle = mock.MagicMock()
     notify = mock.MagicMock()
     monkeypatch.setattr('time.time', lambda: fancysleep.now)
-    fancysleep.patch('pirage.garage.sleep')
+    #fancysleep.patch('pirage.garage.sleep')
 
     g = Garage(toggle)
     g.notify = notify
@@ -295,16 +305,16 @@ def test_garage_notify_canceled_after_close(fancysleep, monkeypatch):
     g.update(AttrDict(pir=False, mag=True))
 
     with fancysleep:
-        gevent.sleep(5)
+        await aio.sleep(5)
 
         # door open
         g.update(AttrDict(pir=True, mag=False))
 
-        gevent.sleep(g.notify_delay-20)
+        await aio.sleep(g.notify_delay-20)
         assert mock.call("Garage is still open after {mag_open} minutes!") not in notify.mock_calls
 
         # close door
         g.update(AttrDict(pir=True, mag=True))
-        gevent.sleep(300)
+        await aio.sleep(300)
         assert mock.call("Garage is still open after {mag_open} minutes!") not in notify.mock_calls
         print ('WTF',notify.mock_calls)
--- pirage/tests/test_hardware.py
+++ pirage/tests/test_hardware.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import
 from mock import MagicMock
 import pytest
 
-import gevent
-from gevent import spawn, sleep
+#import gevent
+#from gevent import spawn, sleep
+import asyncio as aio
 
-from .fastsleep import fastsleep
+#from .fastsleep import fastsleep
+from .aiofastsleep import fastsleep
 
 from pirage.hardware import Monitor
 
@@ -22,10 +24,11 @@ def gpio(monkeypatch):
 
     return io
 
-def test_modifying_update_result_doesnt_break(fastsleep, gpio):
+@pytest.mark.asyncio
+async def test_modifying_update_result_doesnt_break(fastsleep, gpio):
         from pirage import hardware as hw
         from itertools import cycle
-        fastsleep.patch('pirage.hardware.sleep')
+        #fastsleep.patch('pirage.hardware.sleep')
 
         # mag swithc toggles
         gpio.input.side_effect = cycle((gpio.LOW,gpio.HIGH))
@@ -38,26 +41,27 @@ def test_modifying_update_result_doesnt_break(fastsleep, gpio):
         mm.callback.side_effect = mod
         m.register(mm.callback)
         m.start()
-        gevent.sleep() # no change L / H
+        await aio.sleep() # no change L / H
         m.ignore_pir = True # disable pir input
-        gevent.sleep() # change L / L
+        await aio.sleep() # change L / L
         # hack, since mock doesn't keep immutable call args
         assert m._current == {'pir':gpio.LOW, 'mag': gpio.LOW}
-        gevent.sleep() # change L / H
+        await aio.sleep() # change L / H
         assert m._current == {'pir':gpio.LOW, 'mag': gpio.HIGH}
-        gevent.sleep() # change L / L
+        await aio.sleep() # change L / L
         assert m._current == {'pir':gpio.LOW, 'mag': gpio.LOW}
-        m.stop()
+        await m.stop()
 
         assert len(mm.callback.mock_calls) == 4
 
-def test_no_pir(fastsleep, gpio):
+@pytest.mark.asyncio
+async def test_no_pir(fastsleep, gpio):
     '''
     make sure we ignore pir sensor if disabled
     '''
     from pirage import hardware as hw
     from itertools import cycle
-    fastsleep.patch('pirage.hardware.sleep')
+    #fastsleep.patch('pirage.hardware.sleep')
 
     # we only read door pin
     gpio.input.side_effect = cycle((gpio.LOW,))
@@ -68,23 +72,24 @@ def test_no_pir(fastsleep, gpio):
     m.register(mm.callback)
 
     m.start()
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
     gpio.input.assert_called_with(hw._mag_pin)
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
     gpio.input.assert_called_with(hw._mag_pin)
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
     gpio.input.assert_called_with(hw._mag_pin)
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
     gpio.input.assert_called_with(hw._mag_pin)
-    m.stop()
+    await m.stop()
 
     assert len(mm.callback.mock_calls) == 1 # only called once
     assert len(gpio.input.mock_calls) == 4 # only mag is checked
 
-def test_no_pir_door_still_works(fastsleep, gpio):
+@pytest.mark.asyncio
+async def test_no_pir_door_still_works(fastsleep, gpio):
     from pirage import hardware as hw
     from itertools import cycle
-    fastsleep.patch('pirage.hardware.sleep')
+    #fastsleep.patch('pirage.hardware.sleep')
 
     # mag swithc toggles
     gpio.input.side_effect = cycle((gpio.LOW,gpio.HIGH))
@@ -93,32 +98,34 @@ def test_no_pir_door_still_works(fastsleep, gpio):
     m = Monitor()
     m.register(mm.callback)
     m.start()
-    gevent.sleep() # no change L / H
+    await aio.sleep() # no change L / H
     m.ignore_pir = True # disable pir input
-    gevent.sleep() # change L / L
+    await aio.sleep() # change L / L
     mm.callback.assert_called_with({'pir': gpio.LOW, 'mag': gpio.LOW})
-    gevent.sleep() # change L / H
+    await aio.sleep() # change L / H
     mm.callback.assert_called_with({'pir': gpio.LOW, 'mag': gpio.HIGH})
-    gevent.sleep() # change L / L
+    await aio.sleep() # change L / L
     mm.callback.assert_called_with({'pir': gpio.LOW, 'mag': gpio.LOW})
-    m.stop()
+    await m.stop()
 
     assert len(mm.callback.mock_calls) == 4
 
-def test_run_fires_callback_on_start():
+@pytest.mark.asyncio
+async def test_run_fires_callback_on_start():
     mm = MagicMock()
     m = Monitor()
     m.register(mm.callback)
 
     m.start()
-    sleep(0) # let greenlet run once
-    m.stop()
+    await aio.sleep(0) # let greenlet run once
+    await m.stop()
     assert mm.callback.called
     assert len(mm.callback.mock_calls) == 1
 
-def test_run_fires_callback_on_change(fastsleep, gpio):
+@pytest.mark.asyncio
+async def test_run_fires_callback_on_change(fastsleep, gpio):
     from itertools import cycle
-    fastsleep.patch('pirage.hardware.sleep')
+    #fastsleep.patch('pirage.hardware.sleep')
 
     # no changes on input
     gpio.input.side_effect = cycle((gpio.HIGH,gpio.LOW))
@@ -128,27 +135,29 @@ def test_run_fires_callback_on_change(fastsleep, gpio):
     m.register(mm.callback)
 
     m.start()
-    gevent.sleep() # let greenlet run once
-    gevent.sleep() # let greenlet run once
-    gevent.sleep() # let greenlet run once
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
 
     assert len(mm.callback.mock_calls) == 1 # only called once
 
     # make changes (after first call)
     gpio.input.side_effect = cycle((gpio.HIGH,gpio.LOW,gpio.LOW))
 
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
 
     # no extra calls
     assert len(mm.callback.mock_calls) == 1
 
-    gevent.sleep() # let greenlet run once
-    gevent.sleep() # let greenlet run once
-    gevent.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
+    await aio.sleep() # let greenlet run once
 
     # 3 new calls
     assert len(mm.callback.mock_calls) == 4
+
+    await m.stop()
 
 def test_relay_starts_high(gpio):
     from pirage import hardware as hw
@@ -156,16 +165,17 @@ def test_relay_starts_high(gpio):
     # gpio.output.assert_called_once_with(hw._relay_pin, gpio.HIGH)
     gpio.setup.assert_called_with(hw._relay_pin, gpio.OUT, initial=gpio.HIGH)
 
-def test_toggle_relay(fastsleep, gpio):
-    fastsleep.patch('pirage.hardware.sleep')
+@pytest.mark.asyncio
+async def test_toggle_relay(fastsleep, gpio):
+    #fastsleep.patch('pirage.hardware.sleep')
     from pirage import hardware as hw
 
     m = Monitor()
     gpio.output.reset_mock()
     # put it in a greenlet so we can step it
-    spawn(m.toggle_relay)
-    gevent.sleep() # step to pause
+    aio.ensure_future(m.toggle_relay())
+    await aio.sleep() # step to pause
 
     gpio.output.assert_called_once_with(hw._relay_pin, gpio.LOW)
-    gevent.sleep() # step
+    await aio.sleep() # step
     gpio.output.assert_called_with(hw._relay_pin, gpio.HIGH)
